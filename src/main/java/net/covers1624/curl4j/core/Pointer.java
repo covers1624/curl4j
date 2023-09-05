647b669641974f25369f6d17740533d0b9f745e4
covers1624 <covers1624@gmail.com>
2023-09-05 20:27:49 +0930

    Add Memory/Pointer utils for size_t

--- src/main/java/net/covers1624/curl4j/core/Pointer.java
+++ src/main/java/net/covers1624/curl4j/core/Pointer.java
@@ -20,6 +20,7 @@ public class Pointer {
     public float readFloat() { return Memory.getFloat(address); }
     public double readDouble() { return Memory.getDouble(address); }
     public long readCLong() { return Memory.getCLong(address); }
+    public long readSizeT() { return Memory.getSizeT(address); }
     public long readAddress() { return Memory.getAddress(address); }
     public Pointer readPointer() { return new Pointer(readAddress()); }
     public String readUtf8Safe() { return Memory.readUtf8(address); }
