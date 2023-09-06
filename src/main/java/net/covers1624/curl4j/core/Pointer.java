80852fb46ddcfa25bca39eb7a3e9e4ee9dbc11f9
covers1624 <covers1624@gmail.com>
2023-09-06 17:49:13 +0930

    Support Pointer optionally having a backing ByteBuffer

--- src/main/java/net/covers1624/curl4j/core/Pointer.java
+++ src/main/java/net/covers1624/curl4j/core/Pointer.java
@@ -1,14 +1,39 @@
 package net.covers1624.curl4j.core;
 
+import org.jetbrains.annotations.Nullable;
+
+import java.nio.ByteBuffer;
+
 /**
  * Created by covers1624 on 16/8/23.
  */
 public class Pointer {
 
+    // Used for pointers which require automatic native memory cleanup.
+    public final @Nullable ByteBuffer buf;
     public final long address;
 
+    /**
+     * New pointer with an address not managed by Java.
+     *
+     * @param address The address.
+     */
     public Pointer(long address) {
+        buf = null;
         this.address = address;
+    }
+
+    /**
+     * New pointer with an address managed by Java.
+     * <p>
+     * These {@link ByteBuffer} instances should come from {@link ByteBuffer#allocateDirect}
+     *
+     * @param buf The buffer.
+     */
+    public Pointer(ByteBuffer buf) {
+        if (!buf.isDirect()) throw new IllegalArgumentException("Must be a direct buffer");
+        this.buf = buf;
+        address = Memory.getDirectByteBufferAddress(buf);
     }
 
     // @formatter:off
