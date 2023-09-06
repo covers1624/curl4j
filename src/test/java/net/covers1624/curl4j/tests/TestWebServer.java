5301f6949a405e683477fa8b56635d9c55553a73
covers1624 <covers1624@gmail.com>
2023-09-06 22:50:41 +0930

    Add SSL support for the TestWebServer.

--- src/test/java/net/covers1624/curl4j/tests/TestWebServer.java
+++ src/test/java/net/covers1624/curl4j/tests/TestWebServer.java
@@ -1,13 +1,16 @@
 package net.covers1624.curl4j.tests;
 
 import fi.iki.elonen.NanoHTTPD;
+import org.jetbrains.annotations.Nullable;
 
+import javax.net.ssl.KeyManagerFactory;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyStore;
+import java.util.*;
 
 /**
  * Created by covers1624 on 4/9/23.
@@ -15,15 +18,26 @@ import java.util.Map;
 public class TestWebServer extends NanoHTTPD implements AutoCloseable {
 
     private final int port;
+    private final KeyStore keyStore;
     private final Map<String, Handler> funcMap = new HashMap<>();
 
     public TestWebServer() throws IOException {
-        this(getRandomEphemeralPort());
+        this(getRandomEphemeralPort(), null, null);
     }
 
-    private TestWebServer(int port) throws IOException {
+    public TestWebServer(String keystore, @Nullable String password) throws IOException {
+        this(getRandomEphemeralPort(), keystore, password);
+    }
+
+    private TestWebServer(int port, @Nullable String keystore, @Nullable String password) throws IOException {
         super("127.0.0.1", port);
         this.port = port;
+        if (keystore != null) {
+            keyStore = loadKeyStore(keystore, password);
+            makeSecure(makeSSLSocketFactory(keyStore, loadKeyManager(keyStore, password)), null);
+        } else {
+            keyStore = null;
+        }
         start();
     }
 
@@ -31,8 +45,18 @@ public class TestWebServer extends NanoHTTPD implements AutoCloseable {
         funcMap.put(path, func);
     }
 
+    public byte[] getCertBytes(String alias) throws IOException {
+        if (keyStore == null) throw new IllegalStateException("SSL not configured.");
+        try {
+            return toPemCertificate(keyStore.getCertificate(alias).getEncoded());
+        } catch (Throwable ex) {
+            throw new IOException(ex);
+        }
+    }
+
     public String addr(String path) {
-        return "http://127.0.0.1:" + port + path;
+        String proto = keyStore != null ? "https" : "http";
+        return proto + "://127.0.0.1:" + port + path;
     }
 
     @Override
@@ -73,6 +97,38 @@ public class TestWebServer extends NanoHTTPD implements AutoCloseable {
             total += len;
         }
         return bos.toByteArray();
+    }
+
+    private static KeyStore loadKeyStore(String resource, String password) throws IOException {
+        try {
+            KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+            ks.load(TestWebServer.class.getResourceAsStream(resource), password != null ? password.toCharArray() : new char[0]);
+            return ks;
+        } catch (Throwable ex) {
+            throw new IOException(ex);
+        }
+    }
+
+    private static KeyManagerFactory loadKeyManager(KeyStore keyStore, String password) throws IOException {
+        try {
+            KeyManagerFactory factory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            factory.init(keyStore, password.toCharArray());
+            return factory;
+        } catch (Throwable ex) {
+            throw new IOException(ex);
+        }
+    }
+
+    public static byte[] toPemCertificate(byte[] keyData) {
+        List<String> lines = new LinkedList<>();
+        lines.add("-----BEGIN CERTIFICATE-----");
+        String str = Base64.getEncoder().encodeToString(keyData);
+        // lines of max length 64, matches openssl output format.
+        for (int i = 0; i < str.length(); i += 64) {
+            lines.add(str.substring(i, Math.min(i + 64, str.length())));
+        }
+        lines.add("-----END CERTIFICATE-----");
+        return String.join("\n", lines).getBytes(StandardCharsets.UTF_8);
     }
 
     public interface Handler {
