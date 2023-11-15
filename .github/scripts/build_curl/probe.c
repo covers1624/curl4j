b9c5d8f4c3677f3b3fb3f36835d01b4bce02d031
covers1624 <covers1624@gmail.com>
2023-11-15 18:09:10 +1030

    libcurl probe utility for inspecting libcurl binaries.

---
+++ .github/scripts/build_curl/probe.c
@@ -0,0 +1,72 @@
+// Super terrible program to probe a libcurl binary.
+// gcc -ldl -o probe probe.c && ./probe ./install/curl/lib/libcurl.so
+
+#include <sys/stat.h>
+#include <stdio.h>
+#include <dlfcn.h>
+
+void *libcurl = NULL;
+
+void *curlSym(const char *sym) {
+	void *func = dlsym(libcurl, sym);
+	if (!func) {
+		printf("Could not find symbol '%s'\n", sym);
+		return NULL;
+	}
+	return func;
+}
+
+void print_string_list(char **);
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		printf("Usage: %s <libcurl.so>\n", argv[0]);
+		return 1;
+	}
+
+	char *libcurlPath = argv[1];
+
+	struct stat st;
+	if (stat(libcurlPath, &st) != 0) {
+		printf("Provided file does not exist or is not accessible.\n");
+		return 1;
+	}
+	printf("Testing libcurl at: %s\n", argv[1]);
+
+	libcurl = dlopen(libcurlPath, RTLD_LAZY);
+	if (!libcurl) {
+		printf("Failed to open library: %s\n", dlerror());
+		return 1;
+	}
+
+	void *f_curl_version = curlSym("curl_version");
+	if (!f_curl_version) return 1;
+
+	void *f_curl_version_info = curlSym("curl_version_info");
+	if (f_curl_version_info == NULL) return 1;
+
+	const char *feature_versions = ((const char *(*)(void)) f_curl_version)();
+	void **curl_version_info = ((void **(*)(int)) f_curl_version_info)(10);
+	char *version = curl_version_info[1];
+	char *host = curl_version_info[3];
+
+	printf("libcurl %s (%s) %s\n", version, host, feature_versions);
+	printf("Protocols: ");
+	print_string_list(curl_version_info[8]);
+	printf("\n");
+	printf("Features: ");
+	print_string_list(curl_version_info[25]);
+	printf("\n");
+
+	dlclose(libcurl);
+}
+
+void print_string_list(char **list) {
+	char **ptr = list;
+	while (*ptr != NULL) {
+		if (ptr != list) {
+			printf(" ");
+		}
+		printf("%s", *ptr++);
+	}
+}
